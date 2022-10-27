27964213ec6c9b43bb30cc1ec67496fb21ae4bc9
Théo Fargeas <theofargeas@MacBook-Pro-de-Theo.local>
2022-10-26 20:55:18 -0700

    v8

--- OutsideApplication/src/main/java/com/outside/ApplicationDevHelpers.java
+++ OutsideApplication/src/main/java/com/outside/ApplicationDevHelpers.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * ApplicationDevHelpers <b>class</b>
+ * ApplicationDevHelpers <b>class</b>.
  *
  * @see Annotation
  */

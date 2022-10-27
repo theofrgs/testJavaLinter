5c0e30f93171d10e2b25b3d9fcf31c9737a3cf7f
Théo Fargeas <theofargeas@MacBook-Pro-de-Theo.local>
2022-10-26 20:51:30 -0700

    v7

--- OutsideApplication/src/main/java/com/outside/ApplicationDevHelpers.java
+++ OutsideApplication/src/main/java/com/outside/ApplicationDevHelpers.java
@@ -15,7 +15,6 @@ public class ApplicationDevHelpers {
      * Doubles the value.
      * The long and detailed explanation what the method does.
      *
-     * @param value for doubling.
      * @return double value.
      * */
     @GetMapping("/ping")

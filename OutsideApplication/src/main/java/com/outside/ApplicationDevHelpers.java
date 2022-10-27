4e0a0df7f6009e70872bac23119f16f5d2cb28cf
Théo Fargeas <theofargeas@MacBook-Pro-de-Theo.local>
2022-10-26 20:34:34 -0700

    v3

--- OutsideApplication/src/main/java/com/outside/ApplicationDevHelpers.java
+++ OutsideApplication/src/main/java/com/outside/ApplicationDevHelpers.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApplicationDevHelpers {
 
+    /**
+     * pingRoute
+     */
     @GetMapping("/ping")
     public String pingRoute() {
         return "pong";
